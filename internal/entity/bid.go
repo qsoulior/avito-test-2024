bf665678e6bef12cfa42c9acd0a0582c58b97707
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-07 22:54:34 +0300

    Create additional fields in tender and bid

--- internal/entity/bid.go
+++ internal/entity/bid.go
@@ -1,5 +1,7 @@
 package entity
 
+import "time"
+
 type BidStatus string
 
 const (
@@ -11,10 +13,15 @@ const (
 )
 
 type Bid struct {
-	Name            string `json:"name"`
-	Description     string `json:"description"`
-	Status          string `json:"status"`
-	TenderID        string `json:"tenderId"`
-	OrganizationID  string `json:"organizationId"`
-	CreatorUsername string `json:"creatorUsername"`
+	ID              string    `json:"id"`
+	Name            string    `json:"name"`
+	Description     string    `json:"description"`
+	Status          string    `json:"status"`
+	TenderID        string    `json:"tenderId"`
+	OrganizationID  string    `json:"organizationId"`
+	CreatorUsername string    `json:"creatorUsername"`
+	AuthorType      string    `json:"authorType"`
+	AuthorID        string    `json:"authorId"`
+	Version         int       `json:"version"`
+	CreatedAt       time.Time `json:"createdAt"`
 }
--- internal/entity/tender.go
+++ internal/entity/tender.go
@@ -1,5 +1,7 @@
 package entity
 
+import "time"
+
 type TenderStatus string
 
 const (
@@ -8,11 +10,22 @@ const (
 	TenderClosed    TenderStatus = "Closed"
 )
 
+type TenderServiceType string
+
+const (
+	TenderConstruction TenderServiceType = "Construction"
+	TenderDelivery     TenderServiceType = "Delivery"
+	TenderManufacture  TenderServiceType = "Manufacture"
+)
+
 type Tender struct {
+	ID              string       `json:"id"`
 	Name            string       `json:"name"`
 	Description     string       `json:"description"`
 	ServiceType     string       `json:"serviceType"`
 	Status          TenderStatus `json:"status"`
 	OrganizationID  string       `json:"organizationId"`
 	CreatorUsername string       `json:"creatorUsername"`
+	Version         int          `json:"version"`
+	CreatedAt       time.Time    `json:"createdAt"`
 }
