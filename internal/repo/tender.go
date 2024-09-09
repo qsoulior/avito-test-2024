2c301859962f6254fe67d8af2e44cdc5514dde2c
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-09 13:57:51 +0300

    Implement tender repo, add pgx package and implement pgx-wrapper, fix tender entity and migration

---
+++ internal/repo/tender.go
@@ -0,0 +1,22 @@
+package repo
+
+import (
+	"context"
+
+	"git.codenrock.com/avito-testirovanie-na-backend-1270/cnrprod1725732425-team-77001/zadanie-6105/internal/entity"
+	"github.com/google/uuid"
+)
+
+type TenderData struct {
+	Name        *string
+	Description *string
+	ServiceType *entity.TenderServiceType
+}
+
+type Tender interface {
+	Create(ctx context.Context, tender entity.Tender) (*entity.Tender, error)
+	GetByID(ctx context.Context, tenderID uuid.UUID) (*entity.Tender, error)
+	GetByCreatorUsername(ctx context.Context, username string, limit int, offset int) ([]entity.Tender, error)
+	Update(ctx context.Context, tenderID uuid.UUID, username string, data TenderData) (*entity.Tender, error)
+	UpdateStatus(ctx context.Context, tenderID uuid.UUID, username string, status entity.TenderStatus) (*entity.Tender, error)
+}
