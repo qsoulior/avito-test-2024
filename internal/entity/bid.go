64af9a7cfe18adb4d0627e271bae1c3289ca6e72
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-09 16:18:41 +0300

    Implement validation of tender and bid

--- internal/entity/bid.go
+++ internal/entity/bid.go
@@ -1,6 +1,9 @@
 package entity
 
 import (
+	"errors"
+	"fmt"
+	"slices"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,6 +19,8 @@ const (
 	BidRejected  BidStatus = "Rejected"
 )
 
+var BidStatuses = []BidStatus{BidCreated, BidPublished, BidCanceled, BidApproved, BidRejected}
+
 type BidAuthorType string
 
 const (
@@ -23,15 +28,41 @@ const (
 	BidUser         BidAuthorType = "User"
 )
 
+var BidAuthorTypes = []BidAuthorType{BidOrganization, BidUser}
+
 type Bid struct {
 	ID              uuid.UUID     `json:"id"`
 	Name            string        `json:"name"`
 	Description     string        `json:"description"`
-	Status          string        `json:"status"`
+	Status          BidStatus     `json:"status"`
 	TenderID        uuid.UUID     `json:"tenderId"`
 	AuthorType      BidAuthorType `json:"authorType"`
 	AuthorID        uuid.UUID     `json:"authorId"`
 	CreatorUsername string        `json:"creatorUsername"`
 	Version         int           `json:"version"`
 	CreatedAt       time.Time     `json:"createdAt"`
+}
+
+func (b Bid) Validate() error {
+	if len(b.Name) > 100 {
+		return errors.New("name is too long (max 100)")
+	}
+
+	if len(b.Description) > 500 {
+		return errors.New("description is too long (max 500)")
+	}
+
+	if !slices.Contains(BidStatuses, b.Status) {
+		return fmt.Errorf("status must be one of: %v", BidStatuses)
+	}
+
+	if !slices.Contains(BidAuthorTypes, b.AuthorType) {
+		return fmt.Errorf("author type must be one of: %v", BidAuthorTypes)
+	}
+
+	if b.Version < 1 {
+		return errors.New("version must be greater than or equal to 1")
+	}
+
+	return nil
 }
--- internal/entity/tender.go
+++ internal/entity/tender.go
@@ -1,6 +1,9 @@
 package entity
 
 import (
+	"errors"
+	"fmt"
+	"slices"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,6 +17,8 @@ const (
 	TenderManufacture  TenderServiceType = "Manufacture"
 )
 
+var TenderServiceTypes = []TenderServiceType{TenderConstruction, TenderDelivery, TenderManufacture}
+
 type TenderStatus string
 
 const (
@@ -21,6 +26,8 @@ const (
 	TenderPublished TenderStatus = "Published"
 	TenderClosed    TenderStatus = "Closed"
 )
+
+var TenderStatuses = []TenderStatus{TenderCreated, TenderPublished, TenderClosed}
 
 type Tender struct {
 	ID              uuid.UUID         `json:"id"`
@@ -32,4 +39,28 @@ type Tender struct {
 	CreatorUsername string            `json:"creatorUsername"`
 	Version         int               `json:"version"`
 	CreatedAt       time.Time         `json:"createdAt"`
+}
+
+func (t Tender) Validate() error {
+	if len(t.Name) > 100 {
+		return errors.New("name is too long (max 100)")
+	}
+
+	if len(t.Description) > 500 {
+		return errors.New("description is too long (max 500)")
+	}
+
+	if !slices.Contains(TenderServiceTypes, t.ServiceType) {
+		return fmt.Errorf("service type must be one of: %v", TenderServiceTypes)
+	}
+
+	if !slices.Contains(TenderStatuses, t.Status) {
+		return fmt.Errorf("status must be one of: %v", TenderStatuses)
+	}
+
+	if t.Version < 1 {
+		return errors.New("version must be greater than or equal to 1")
+	}
+
+	return nil
 }
