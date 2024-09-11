3b4bf7346b33b2ea5ce23aa0b70046e9f53450e3
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-09-11 13:06:57 +0300

    Fix api paths

--- internal/transport/http/mux.go
+++ internal/transport/http/mux.go
@@ -14,24 +14,24 @@ func NewMux(tenderService service.Tender, bidService service.Bid, logger *slog.L
 	}
 
 	router := http.NewServeMux()
-	router.Handle("GET /ping", handler.Ping{})
+	router.Handle("GET /api/ping", handler.Ping{})
 
-	router.Handle("GET /tenders", handler.TenderGetByServiceType{Service: tenderService})
-	router.Handle("POST /tenders/new", handler.TenderCreate{Service: tenderService})
-	router.Handle("GET /tenders/my", handler.TenderGetByCreator{Service: tenderService})
-	router.Handle("GET /tenders/{tenderId}/status", handler.TenderGetStatus{Service: tenderService})
-	router.Handle("PUT /tenders/{tenderId}/status", handler.TenderUpdateStatus{Service: tenderService})
-	router.Handle("PATCH /tenders/{tenderId}/edit", handler.TenderUpdate{Service: tenderService})
-	router.Handle("PUT /tenders/{tenderId}/rollback/{version}", handler.TenderRollback{Service: tenderService})
+	router.Handle("GET /api/tenders", handler.TenderGetByServiceType{Service: tenderService})
+	router.Handle("POST /api/tenders/new", handler.TenderCreate{Service: tenderService})
+	router.Handle("GET /api/tenders/my", handler.TenderGetByCreator{Service: tenderService})
+	router.Handle("GET /api/tenders/{tenderId}/status", handler.TenderGetStatus{Service: tenderService})
+	router.Handle("PUT /api/tenders/{tenderId}/status", handler.TenderUpdateStatus{Service: tenderService})
+	router.Handle("PATCH /api/tenders/{tenderId}/edit", handler.TenderUpdate{Service: tenderService})
+	router.Handle("PUT /api/tenders/{tenderId}/rollback/{version}", handler.TenderRollback{Service: tenderService})
 
-	router.Handle("POST /bids/new", handler.BidCreate{Service: bidService})
-	router.Handle("GET /bids/my", handler.BidGetByCreator{Service: bidService})
-	router.Handle("GET /bids/{tenderId}/list", handler.BidGetByTender{Service: bidService})
-	router.Handle("GET /bids/{bidId}/status", handler.BidGetStatus{Service: bidService})
-	router.Handle("PUT /bids/{bidId}/status", handler.BidUpdateStatus{Service: bidService})
-	router.Handle("PATCH /bids/{bidId}/edit", handler.BidUpdate{Service: bidService})
-	router.Handle("PUT /bids/{bidId}/submit_decision", handler.BidSubmitDecision{Service: bidService})
-	router.Handle("PUT /bids/{bidId}/rollback/{version}", handler.BidRollback{Service: bidService})
+	router.Handle("POST /api/bids/new", handler.BidCreate{Service: bidService})
+	router.Handle("GET /api/bids/my", handler.BidGetByCreator{Service: bidService})
+	router.Handle("GET /api/bids/{tenderId}/list", handler.BidGetByTender{Service: bidService})
+	router.Handle("GET /api/bids/{bidId}/status", handler.BidGetStatus{Service: bidService})
+	router.Handle("PUT /api/bids/{bidId}/status", handler.BidUpdateStatus{Service: bidService})
+	router.Handle("PATCH /api/bids/{bidId}/edit", handler.BidUpdate{Service: bidService})
+	router.Handle("PUT /api/bids/{bidId}/submit_decision", handler.BidSubmitDecision{Service: bidService})
+	router.Handle("PUT /api/bids/{bidId}/rollback/{version}", handler.BidRollback{Service: bidService})
 
 	var mux http.Handler = router
 	middlewares := []Middleware{RecovererMiddleware(logger), LoggerMiddleware(logger)}
